6922a719436c4c9c97d8c52103fa7572fbdef873
Daniel Barreto <daniel.barreto717@gmail.com>
2018-02-10 17:08:40 -0500

    use strings in winning scenarions instead of numbers

--- src/utils/decider.js
+++ src/utils/decider.js
@@ -1,39 +1,26 @@
 // @flow
 import * as Decisions from './decisions';
-
-import type { GameChoice } from './choices';
+import * as Choices from './choices';
 
 type WinningScenarioMap = {
-    [choice: GameChoice]: Array<number>
-};
-
-type ChoiceValues = {
-    [choice: GameChoice]: number
-};
-
-const Values: ChoiceValues = {
-    rock: 0,
-    paper: 1,
-    scissors: 2,
-    spock: 3,
-    lizard: 4
+    [choice: Choices.GameChoice]: Array<Choices.GameChoice>
 };
 
 const winningScenarios: WinningScenarioMap = {
-    rock: [Values.scissor, Values.lizard],
-    paper: [Values.rock, Values.spock],
-    scissors: [Values.paper, Values.lizard],
-    spock: [Values.rock, Values.scissors],
-    lizard: [Values.spock, Values.paper]
+    [Choices.ROCK]: [Choices.SCISSORS, Choices.LIZARD],
+    [Choices.PAPER]: [Choices.ROCK, Choices.SPOCK],
+    [Choices.SCISSORS]: [Choices.PAPER, Choices.LIZARD],
+    [Choices.SPOCK]: [Choices.ROCK, Choices.SCISSORS],
+    [Choices.LIZARD]: [Choices.SPOCK, Choices.PAPER]
 };
 
 export function getDecision(
-    playerDecision: GameChoice,
-    opponentDecision: GameChoice
+    playerDecision: Choices.GameChoice,
+    opponentDecision: Choices.GameChoice
 ): Decisions.Decision {
-    if (winningScenarios[playerDecision].includes(Values[opponentDecision])) {
+    if (winningScenarios[playerDecision].includes(opponentDecision)) {
         return Decisions.WIN;
-    } else if (winningScenarios[opponentDecision].includes(Values[playerDecision])) {
+    } else if (winningScenarios[opponentDecision].includes(playerDecision)) {
         return Decisions.LOSE;
     }
     return Decisions.DRAW;
